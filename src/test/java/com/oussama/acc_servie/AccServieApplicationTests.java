a136daf9df8fe3af41b7699bcf32c6e900c449ce
mreckah <m.reckahwalt@gmail.com>
2025-10-26 14:12:40 +0100

    Project Setup

---
+++ src/test/java/com/oussama/acc_servie/AccServieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oussama.acc_servie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccServieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
